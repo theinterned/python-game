a2dc01be72da3cbe948cf87ec20b9dffeb4ddad7
Ned Schwartz <ned@theinterned.net>
2022-10-16 12:20:53 -0400

    start drawing a smiley face 🙂

---
+++ sprites/smiley.py
@@ -0,0 +1,14 @@
+import arcade
+
+# Set constants for the screen size
+# SCREEN_WIDTH = 600
+# SCREEN_HEIGHT = 600
+Cheeseburger = 600
+Pizza = 600
+
+arcade.open_window(Cheeseburger, Pizza, "Smiley Face")
+
+# Set the background color
+arcade.set_background_color(arcade.color.WHITE)
+
+arcade.run()
