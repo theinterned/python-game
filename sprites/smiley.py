fc35e349f2c227ab86465fb6f30984105100ec7b
Ned Schwartz <ned@theinterned.net>
2022-10-16 12:27:44 -0400

    use better constant names

--- sprites/smiley.py
+++ sprites/smiley.py
@@ -1,14 +1,16 @@
 import arcade
 
 # Set constants for the screen size
-# SCREEN_WIDTH = 600
-# SCREEN_HEIGHT = 600
-Cheeseburger = 600
-Pizza = 600
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 
-arcade.open_window(Cheeseburger, Pizza, "Smiley Face")
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Smiley Face")
 
-# Set the background color
+# Set the background color to white.
+# For a list of named colors see:
+# http://arcade.academy/arcade.color.html
+# Colors can also be specified in (red, green, blue) format and
+# (red, green, blue, alpha) format.
 arcade.set_background_color(arcade.color.WHITE)
 
 arcade.run()
