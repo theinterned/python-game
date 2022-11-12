acc3cbfe72d413e2febbda727f8551684a8ba6f7
Ned Schwartz <ned@theinterned.net>
2022-11-12 16:23:29 -0500

    draw a very nice smiley face

--- sprites/smiley.py
+++ sprites/smiley.py
@@ -15,6 +15,39 @@ def main():
     # (red, green, blue, alpha) format.
     arcade.set_background_color(arcade.color.WHITE)
 
+    arcade.start_render()
+
+    # draw face
+    x = 300
+    y = 300
+    radius = 200
+    arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+
+    #  draw right eye
+    x = 370
+    y = 370
+    radius = 70
+    arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+
+    #  draw left eye
+    x = 220
+    y = 370
+    radius = 20
+    arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+
+    # draw smile
+    x = 300
+    y = 220
+    width = 500
+    height = 100
+    start_angle = 190
+    end_angle = 350
+    thickness = 10
+    arcade.draw_arc_outline(
+        x, y, width, height, arcade.color.BLACK, start_angle, end_angle, thickness)
+
+    arcade.finish_render()
+
     arcade.run()
 
 
